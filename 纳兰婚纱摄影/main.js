30295a9bb63493348b3ee3927a9351f89afdf068
Gszx-chang <2428567220@qq.com>
2016-11-24 10:50:54 +0800

    Update main.js

--- 纳兰婚纱摄影/main.js
+++ 纳兰婚纱摄影/main.js
@@ -1,5 +1,16 @@
-window.onscroll=function(){
-	var ask=document.getElementById('weixin');
-	ask.style.top=document.body.scrollTop+125+"px";
-	console.log(5);
-}
+
+window.onload = function(){
+    var oTop = document.getElementById("weixin");
+    var screenw = document.documentElement.clientWidth || document.body.clientWidth;
+    var screenh = document.documentElement.clientHeight || document.body.clientHeight;
+    oTop.style.left = screenw - oTop.offsetWidth - 10 + "px";
+    oTop.style.top = screenh - oTop.offsetHeight - 30 + "px";
+    window.onscroll = function(){
+        var scrolltop = document.documentElement.scrollTop || document.body.scrollTop;
+        oTop.style.top = screenh - oTop.offsetHeight + scrolltop -30 +"px";
+    }
+}  
+
+function gettop(){
+    document.documentElement.scrollTop = document.body.scrollTop =0;
+  }
